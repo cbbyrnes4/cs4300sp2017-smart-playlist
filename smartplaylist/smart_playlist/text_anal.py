856d2e359e56c208ebfcdb4890179bb21f9db892
Ian Blakley <ianhblakley@gmail.com>
2017-04-16 16:56:59 -0400

    Added initial search functionality and bug fixes

--- smartplaylist/smart_playlist/text_anal.py
+++ smartplaylist/smart_playlist/text_anal.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-from django.smart_playlist.models import Lyric, Song, Word
+from smart_playlist.models import Lyric, Song, Word
 
 
-def build_df_matrix(song_count):
+def build_df_matrix():
     """
     Returns the 
     :return: 
     """
-    df = np.zeros(song_count)
+    df = np.zeros(Word.objects.count())
     for word in Word.objects.all():
         df[word.id - 1] = word.lyric_set.count() + 1
     return df
@@ -23,13 +23,13 @@ def build_song_term_matrix(song_count, doc_freq):
     lyrics = Lyric.objects.all()
     mat = np.zeros([Song.objects.count(), Word.objects.count()])
     for lyric in lyrics:
-        mat[lyric.song_id - 1, lyric.word_id - 1] = float(lyric.count) / np.log(
+        mat[lyric.song_id - 1, lyric.word_id - 1] = float(lyric.count) * np.log(
             song_count / doc_freq[lyric.word_id - 1])
     return mat
 
 
-song_count = Song.objects.count()
-doc_freq = build_df_matrix(song_count)
+song_count = Song.objects.count() + 1
+doc_freq = build_df_matrix()
 song_word = build_song_term_matrix(song_count, doc_freq)
 
 
@@ -41,21 +41,25 @@ def get_lyrically_overlapping_songs(song):
     """
     song_words = Lyric.objects.filter(song=song).values('word').distinct()
     lyrics = Lyric.objects.filter(word__in=song_words)
-    songs_set = lyrics.values('song_id').distinct()
+    songs_set = lyrics.exclude(song=song).values('song_id').distinct()
     return songs_set
 
 
 def get_top_songs(song):
     overlap = get_lyrically_overlapping_songs(song)
     cos_sims = calc_cosine_sims(song, overlap)
-    top_indices = np.argsort(cos_sims)[-min(len(cos_sims), 15):]
-    return [overlap[i] for i in top_indices]
+    top_indices = np.argsort(cos_sims)[::-1][-min(len(cos_sims), 15):]
+    return [(Song.objects.get(id=overlap[i]['song_id']), cos_sims[i]) for i in top_indices]
 
 
 def calc_cosine_sims(song, overlapping_songs):
     song_tf = np.zeros(Word.objects.count())
-    for lyric in song.lyric_set:
-        song_tf[lyric.word_id] = float(lyric.count) / np.log(song_count / doc_freq[lyric.word_id - 1])
+    for lyric in song.lyric_set.all():
+        try:
+            df = doc_freq[lyric.word.id-1]
+        except IndexError:
+            df = 1
+        song_tf[lyric.word_id-1] = float(lyric.count) * np.log(song_count / df)
     cos_sims = np.zeros(len(overlapping_songs))
     for ind, olap in enumerate(overlapping_songs):
         cos_sims[ind] = cosine_sim(song_tf, olap)
@@ -63,4 +67,6 @@ def calc_cosine_sims(song, overlapping_songs):
 
 
 def cosine_sim(q, d):
-    return np.dot(q, song_word[d.id]) / (np.linalg.norm(q) * np.linalg.norm(song_word[d.id]))
+    song_vec = song_word[d['song_id'] - 1]
+    song_vec = np.append(song_vec, np.zeros(len(q)-len(song_vec)))
+    return np.dot(q, song_vec) / (np.linalg.norm(q) * np.linalg.norm(song_vec))
