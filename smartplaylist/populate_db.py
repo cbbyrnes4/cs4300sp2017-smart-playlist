685bea3ab7477b6c7da54fc4f2ebfff3333d3818
Chaz Byrnes <chazbyrnes@Chazs-MacBook-Pro.local>
2017-04-17 00:58:31 -0400

    changes

--- smartplaylist/populate_db.py
+++ smartplaylist/populate_db.py
@@ -6,11 +6,13 @@ from spotipy import SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials
 
 if __name__ == '__main__':
-    spotify_key = 'd305abf923f54ff0b2bee3ae17af289d'
-    spotify_secret = '55b73d4d03a44a309973c0693edbeaf9'
+	spotify_key = 'd305abf923f54ff0b2bee3ae17af289d'
+	spotify_secret = '55b73d4d03a44a309973c0693edbeaf9'
 
-    spo_cred_manager = SpotifyClientCredentials(spotify_key, spotify_secret)
-    sp = spotipy.Spotify(client_credentials_manager=spo_cred_manager)
+	spo_cred_manager = SpotifyClientCredentials(spotify_key, spotify_secret)
+	sp = spotipy.Spotify(client_credentials_manager=spo_cred_manager)
+
+	global category
 
     for category in sp.categories(limit=50)['categories']['items']:
         db_builder.fetch_category_playlists(category['id'])
