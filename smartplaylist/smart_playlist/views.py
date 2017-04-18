1052921d17a45fbf217e980255056b6dc171d20a
Ian Blakley <ianhblakley@gmail.com>
2017-04-18 16:00:41 -0400

    Moved Building matrices out of request code Loads memoryies from pickle file instead

--- smartplaylist/smart_playlist/views.py
+++ smartplaylist/smart_playlist/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 from smart_playlist import db_builder, text_anal
@@ -9,14 +9,16 @@ from smart_playlist import db_builder, text_anal
 def search(request):
     output = ''
     if not text_anal.initialized:
-        text_anal.build_matrices()
+        return HttpResponse("Loading Data")
     if request.GET.get('song'):
         song = request.GET.get('song')
         artist = request.GET.get('artist')
         song, created = db_builder.build_song_from_name(song, artist)
         if created:
             text_anal.refresh_matrices(song)
-        top_songs = text_anal.get_top_songs(song)
+            top_songs = text_anal.get_cosine_top_songs(song)
+        else:
+            top_songs = text_anal.get_pmi_top_songs(song)
         paginator = Paginator(top_songs, 10)
         page = request.GET.get('page')
         try:
