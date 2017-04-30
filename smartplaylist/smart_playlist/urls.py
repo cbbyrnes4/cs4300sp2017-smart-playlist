d87f338dc351559ab5d87ab30377979771028a97
Ian Blakley <ianhblakley@gmail.com>
2017-04-30 14:03:46 -0400

    Ajax working

--- smartplaylist/smart_playlist/urls.py
+++ smartplaylist/smart_playlist/urls.py
@@ -4,6 +4,6 @@ from smart_playlist import views
 
 app_name = 'sp'
 urlpatterns = [
-    url(r'$', views.search, name="search"),
+    url(r'^$', views.search, name="search"),
     url(r'^find_song/$', views.find_song, name='find_song')
 ]
