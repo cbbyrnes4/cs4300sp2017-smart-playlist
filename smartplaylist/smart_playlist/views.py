82b04561bb7a25faf81bd595fe4580506d7dadab
Chaz Byrnes <chazbyrnes@Chazs-MacBook-Pro.local>
2017-05-01 02:43:42 -0400

    more ui changes

--- smartplaylist/smart_playlist/views.py
+++ smartplaylist/smart_playlist/views.py
@@ -22,6 +22,9 @@ spotify_secret = '55b73d4d03a44a309973c0693edbeaf9'
 spo_cred_manager = SpotifyClientCredentials(spotify_key, spotify_secret)
 sp = spotipy.Spotify(client_credentials_manager=spo_cred_manager)
 
+keys = {0: 'C', 1: 'C#', 2: 'D', 3: 'D#', 4: 'E', 5: 'F', 6: 'F#', 7: 'G', 8: 'G#', 9: 'A', 10: 'A#', 11: 'B'}
+
+
 def search(request):
     query_song = None
     songs = None
